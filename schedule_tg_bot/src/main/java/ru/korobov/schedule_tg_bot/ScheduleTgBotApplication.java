a574d2530558c0117589327f17bbe1f9d341d21d
KorobovAndrew <AndrewBoxMail@yandex.ru>
2023-02-27 21:44:35 +0300

    Телеграм эхо-бот

---
+++ schedule_tg_bot/src/main/java/ru/korobov/schedule_tg_bot/ScheduleTgBotApplication.java
@@ -0,0 +1,13 @@
+package ru.korobov.schedule_tg_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleTgBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleTgBotApplication.class, args);
+    }
+
+}
---
+++ schedule_tg_bot/src/test/java/ru/korobov/schedule_tg_bot/ScheduleTgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.korobov.schedule_tg_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleTgBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
